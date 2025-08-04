91c83c8df3e62113b11da28a74f86cef7b2fac89
RonaldJoyTengco <RonaldJoyTengco@yahoo.com>
2025-08-04 10:12:34 +0800

    Added Hamburger Menu

--- app/Views/common/admin_menubar.php
+++ app/Views/common/admin_menubar.php
@@ -7,6 +7,14 @@
             <div class="container-fluid">
                 <!--begin::Start Navbar Links-->
 
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                            <i class="bi bi-list"></i>
+                        </a>
+                    </li>
+                </ul>
+
                 <!--end::Start Navbar Links-->
                 <!--begin::End Navbar Links-->
                 <ul class="navbar-nav ms-auto">
