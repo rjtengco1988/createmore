9c50918613cfb9c09d2a91ca55904529abfceb0e
RonaldJoyTengco <RonaldJoyTengco@yahoo.com>
2025-08-04 13:17:20 +0800

    Created migration file for permissions table

---
+++ app/Database/Migrations/2025-08-04-042353_CreatePermissionsTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePermissionsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name'    => ['type' => 'VARCHAR', 'constraint' => '100'],
+            'slug'       => ['type' => 'VARCHAR', 'constraint' => '100'],
+            'description'    => ['type' => 'TEXT'],
+            'created_at' => [
+                'type'    => 'TIMESTAMP',
+                'null'    => true
+            ],
+            'updated_at'  => ['type' => 'TIMESTAMP', 'null' => true],
+            'deleted_at'  => ['type' => 'TIMESTAMP', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('acl_permissions');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('acl_permissions');
+    }
+}
